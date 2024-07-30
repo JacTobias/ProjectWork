ad97c2949d5db18c0bbc375af2e1e388baf6bea1
Jacqueline <jactobias63@gmail.com>
2024-07-30 16:59:16 -0400

    added customerlist

---
+++ front-end-app/src/CustomerList.js
@@ -0,0 +1,16 @@
+
+export function CustomerList(params){
+    return(
+        <div className="boxed">
+        <h4>Customer List</h4>
+        <table border='1px black'>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Password</td>
+            </tr>
+        </table>
+        </div>
+    );
+};
+
